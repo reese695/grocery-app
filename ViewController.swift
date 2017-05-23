a2c175562a166dc16de930454a1475c569ca67b7
Reese Woodard <reese695@gmail.com>
2017-05-23 08:24:31 -0700

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  grocery-app
+//
+//  Created by Reese Woodard on 5/23/17.
+//  Copyright © 2017 Reese Woodard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
